a57a28eb30b66399cae205cb2f5d0b24221505ad
LolitaWu <1026705120@qq.com>
2020-01-28 22:54:10 +0800

    index on master: e920340 收藏

---
+++ miniprogram/vant/packages/popup/index.ts
@@ -0,0 +1,71 @@
+import { VantComponent } from '../common/component';
+import { transition } from '../mixins/transition';
+import { safeArea } from '../mixins/safe-area';
+
+VantComponent({
+  classes: [
+    'enter-class',
+    'enter-active-class',
+    'enter-to-class',
+    'leave-class',
+    'leave-active-class',
+    'leave-to-class'
+  ],
+
+  mixins: [transition(false), safeArea()],
+
+  props: {
+    round: Boolean,
+    customStyle: String,
+    overlayStyle: String,
+    transition: {
+      type: String,
+      observer: 'observeClass'
+    },
+    zIndex: {
+      type: Number,
+      value: 100
+    },
+    overlay: {
+      type: Boolean,
+      value: true
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      value: true
+    },
+    position: {
+      type: String,
+      value: 'center',
+      observer: 'observeClass'
+    }
+  },
+
+  created() {
+    this.observeClass();
+  },
+
+  methods: {
+    onClickOverlay() {
+      this.$emit('click-overlay');
+
+      if (this.data.closeOnClickOverlay) {
+        this.$emit('close');
+      }
+    },
+
+    observeClass() {
+      const { transition, position } = this.data;
+
+      const updateData: { [key: string]: any } = {
+        name: transition || position
+      };
+
+      if (transition === 'none') {
+        updateData.duration = 0;
+      }
+
+      this.set(updateData);
+    }
+  }
+});
