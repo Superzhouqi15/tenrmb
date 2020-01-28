e92034048e1237c0b717c52b3fede1c7109ffdba
LolitaWu <1026705120@qq.com>
2020-01-28 22:28:10 +0800

    收藏

---
+++ miniprogram/iview/dist/card/index.js
@@ -0,0 +1,49 @@
+let isCollect = false
+
+Component({
+    externalClasses: ['i-class'],
+
+    options: {
+        multipleSlots: true
+    },
+
+  data: {
+    collectLogoUrl: "../../../icon/shoucang.png"
+  },
+  
+  methods:{
+    clickCollect:function() {
+      if (isCollect) {
+        this.setData({
+          collectLogoUrl: "../../../icon/shoucang.png"
+        })
+        isCollect=false
+      }
+      else {
+        this.setData({
+          collectLogoUrl: "../../../icon/shoucang1.png"
+        })
+        isCollect=true
+      }
+    }
+  },
+
+    properties: {
+        full: {
+            type: Boolean,
+            value: false
+        },
+        thumb: {
+            type: String,
+            value: ''
+        },
+        title: {
+            type: String,
+            value: ''
+        },
+        extra: {
+            type: String,
+            value: ''
+        }
+    }
+});
