868b0da9eb47e459be42ccca4b2cdcea6e7babc2
Dav1dch <807557852@qq.com>
2020-02-21 14:00:50 +0800

    fix cal objectId

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -266,7 +266,7 @@ App({
     var oId = id.timeSecond.toString(16) +
       id.machineIdentifier.toString(16) +
       id.processIdentifier.toString(16) +
-      id.counter.toString(16);
+      ('000000' + id.counter.toString(16)).slice(-6);
     return oId
   },
 })
